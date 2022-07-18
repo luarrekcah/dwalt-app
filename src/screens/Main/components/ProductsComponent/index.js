6707ed0f0e25213c4181da6345a4898fbaf570ba
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-07-18 18:56:04 -0500

    [ADDED] products preview

--- src/screens/Main/components/ProductsComponent/index.js
+++ src/screens/Main/components/ProductsComponent/index.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import React, {useEffect} from 'react';
+import {View, FlatList, Text, StyleSheet, Dimensions} from 'react-native';
 import {Card, FAB} from 'react-native-paper';
 import Colors from '../../../../globalStyles/colors';
+import database from '@react-native-firebase/database';
+import moment from 'moment';
+moment.locale('pt-br');
 
 const Products = ({navigation}) => {
+  const [db, setDb] = React.useState({});
+
+  useEffect(() => {
+    database()
+      .ref('/dataWebSite/products')
+      .once('value')
+      .then(snapshot => {
+        setDb(snapshot.val());
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Card style={styles.card}>
-        <Card.Cover
-          source={{
-            uri: 'https://cdn.glitch.global/e3790b73-514c-45b3-a987-85c9a4b7481f/JINKO450W.jpg?v=1645209367065',
-          }}
-        />
-        <Text style={styles.title}>KIT GERADOR 200KW - (127477-3)</Text>
-        <Text style={styles.desc}>
-          GF 1,8KWP JINKO TIGER PRO MONO 450W MIC 1.5KW 1MPPT MONO 220V
-        </Text>
-        <Text style={styles.value}>R$8.319,00</Text>
-        <Text style={styles.time}>POSTADO DIA 17 DE FEV 2022</Text>
-      </Card>
+      <FlatList
+        data={db}
+        renderItem={({item}) => (
+          <Card style={styles.card}>
+            <Card.Cover
+              source={{
+                uri: item.mediaSrc[0],
+              }}
+            />
+            <Text style={styles.title}>
+              {item.resumedtitle} - ({item.code})
+            </Text>
+            <Text style={styles.desc}>{item.description}</Text>
+            <Text style={styles.value}>R${item.value}</Text>
+            <Text style={styles.time}>
+              POSTADO HÁ {moment(item.postedDate).fromNow()}
+            </Text>
+          </Card>
+        )}
+      />
+
       <FAB
         icon="plus"
         style={styles.fab}
